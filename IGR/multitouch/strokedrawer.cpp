082efd7ade83716d6f4963f6e10ece2dbc29a045
Benjamin Auclair <bauclair@a406-10.enst.fr>
2016-04-04 11:21:37 +0200

    Fin de l'étape 3

--- IGR/multitouch/strokedrawer.cpp
+++ IGR/multitouch/strokedrawer.cpp
@@ -1,21 +1,36 @@
 #include "strokedrawer.h"
+#include <qmath.h>
 
-StrokeDrawer::StrokeDrawer()
+StrokeDrawer::StrokeDrawer(QPolygonF gesture_new)
 {
+  this->gesture = gesture_new;
 }
 
-void StrokeDrawer::setStroke(QGesture gesture_new) {
+void StrokeDrawer::setStroke(QPolygonF gesture_new) {
     this->gesture = gesture_new;
 }
 
-QGesture StrokeDrawer::getStroke(){
+QPolygonF StrokeDrawer::getStroke(){
     return this->gesture;
 }
 
 void StrokeDrawer::clearStroke(){
-    this->gesture = NULL;
+  this->gesture = QPolygonF();
 }
 
 void StrokeDrawer::displayStroke(){
+  QPainter painter(this);
+  painter.drawPolyline(this->gesture);
+}
 
+QSize StrokeDrawer::sizeHint() const{
+  QRectF r = (this->gesture).boundingRect();
+  int max = qMax(r.width(),r.height());
+  return QSize(max,max);
+}
+
+QSize StrokeDrawer::minimumSizeHint() const{
+  QRectF r = (this->gesture).boundingRect();
+  int max = qMax(r.width(),r.height());
+  return QSize(max,max);
 }
