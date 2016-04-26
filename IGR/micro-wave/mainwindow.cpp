927be6b3e517f8c21277aaf77aaf82ecb0296881
Benjamin <benjamin@awben.rezel.net>
2016-04-26 18:31:57 +0200

    z

--- IGR/micro-wave/mainwindow.cpp
+++ IGR/micro-wave/mainwindow.cpp
@@ -57,7 +57,7 @@ MainWindow::MainWindow(QWidget *parent) :
     setMinute->addTransition(ui->clockButton,SIGNAL(clicked()),defaultState);
 
     heating->addTransition(ui->startButton,SIGNAL(clicked()),heating);
-    addEventTrans(heating,defaultState,this->time,SIGNAL(timeout()));
+    addEventTrans(heating,defaultState,(QObject * ) this->time,SIGNAL(timeout()));
 
     s1->addTransition(ui->stopButton,SIGNAL(clicked()),s1);
 
